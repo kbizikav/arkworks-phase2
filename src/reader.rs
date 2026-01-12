45219e61db96cdad40d5f045215e76215b9d536f
kbizikav <132550763+kbizikav@users.noreply.github.com>
2026-01-12 17:15:30 +0700

    chore: update deps

--- src/reader.rs
+++ src/reader.rs
@@ -1,15 +1,38 @@
 use std::{io::Read, ops::Div};
 
 use ark_ec::{
-    bls12::{Bls12, Bls12Parameters},
-    bn::{Bn, BnParameters},
-    PairingEngine,
+    bls12::{Bls12, Bls12Config},
+    bn::{Bn, BnConfig},
 };
-use ark_ff::{BigInteger, FpParameters, FromBytes, PrimeField, QuadExtField};
+use ark_ec::pairing::Pairing;
+use ark_ff::{BigInteger, PrimeField, QuadExtField};
 
 use crate::error::Error;
 
-pub trait PairingReader: PairingEngine {
+fn read_bigint_le<F: PrimeField>(bytes: &[u8]) -> Result<F::BigInt, Error> {
+    let mut repr = F::BigInt::default();
+    let limbs = repr.as_mut();
+    if bytes.len() != limbs.len() * 8 {
+        return Err(Error::Read(format!(
+            "Unexpected bigint size: {}",
+            bytes.len()
+        )));
+    }
+    for (i, limb) in limbs.iter_mut().enumerate() {
+        let start = i * 8;
+        let mut chunk = [0u8; 8];
+        chunk.copy_from_slice(&bytes[start..start + 8]);
+        *limb = u64::from_le_bytes(chunk);
+    }
+    Ok(repr)
+}
+
+fn montgomery_r<F: PrimeField>() -> F {
+    let exp = 64u64 * <F::BigInt as BigInteger>::NUM_LIMBS as u64;
+    F::from(2u64).pow([exp])
+}
+
+pub trait PairingReader: Pairing {
     fn read_ptau_g1<R: Read>(reader: &mut R) -> Result<Self::G1Affine, Error>;
     fn read_ptau_g2<R: Read>(reader: &mut R) -> Result<Self::G2Affine, Error>;
 
@@ -17,21 +40,21 @@ pub trait PairingReader: PairingEngine {
     fn read_radix_g2<R: Read>(reader: &mut R) -> Result<Self::G2Affine, Error>;
 }
 
-impl<T: BnParameters> PairingReader for Bn<T> {
+impl<T: BnConfig> PairingReader for Bn<T> {
     fn read_ptau_g1<R: Read>(reader: &mut R) -> Result<Self::G1Affine, Error> {
-        let r = Self::Fq::from_repr(<Self::Fq as PrimeField>::Params::R).unwrap();
-        let len = <Self::Fq as PrimeField>::BigInt::NUM_LIMBS * 8;
+        let r = montgomery_r::<Self::BaseField>();
+        let len = <Self::BaseField as PrimeField>::BigInt::NUM_LIMBS * 8;
         let mut b = vec![0; len * 2];
         reader.read_exact(&mut b)?;
 
-        let x = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..len])?)
+        let x = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..len])?)
             .ok_or(Error::Read(String::from("Unable to read Fq x")))?
             .div(r);
-        let y = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[len..])?)
+        let y = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[len..])?)
             .ok_or(Error::Read(String::from("Unable to read Fq y")))?
             .div(r);
 
-        let g1 = Self::G1Affine::new(x, y, false);
+        let g1 = Self::G1Affine::new_unchecked(x, y);
 
         assert!(g1.is_on_curve(), "G1 must be on curve");
 
@@ -39,28 +62,30 @@ impl<T: BnParameters> PairingReader for Bn<T> {
     }
 
     fn read_ptau_g2<R: Read>(reader: &mut R) -> Result<Self::G2Affine, Error> {
-        let r = Self::Fq::from_repr(<Self::Fq as PrimeField>::Params::R).unwrap();
-        let len = <Self::Fq as PrimeField>::BigInt::NUM_LIMBS * 8;
+        let r = montgomery_r::<Self::BaseField>();
+        let len = <Self::BaseField as PrimeField>::BigInt::NUM_LIMBS * 8;
         let mut b = vec![0; len * 4];
         reader.read_exact(&mut b)?;
 
-        let x0 = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..len])?)
+        let x0 = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..len])?)
             .ok_or(Error::Read(String::from("Unable to read Fq x0")))?
             .div(r);
-        let x1 = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[len..(len * 2)])?)
+        let x1 = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(
+            &b[len..(len * 2)],
+        )?)
             .ok_or(Error::Read(String::from("Unable to read Fq x1")))?
             .div(r);
         let x = QuadExtField::new(x0, x1);
-        let y0 = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(
+        let y0 = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(
             &b[(len * 2)..(len * 3)],
         )?)
         .ok_or(Error::Read(String::from("Unable to read Fq y0")))?
         .div(r);
-        let y1 = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[(len * 3)..])?)
+        let y1 = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[(len * 3)..])?)
             .ok_or(Error::Read(String::from("Unable to read Fq y1")))?
             .div(r);
         let y = QuadExtField::new(y0, y1);
-        let g2 = Self::G2Affine::new(x, y, false);
+        let g2 = Self::G2Affine::new_unchecked(x, y);
 
         assert!(g2.is_on_curve(), "G2 must be on curve");
 
@@ -68,23 +93,23 @@ impl<T: BnParameters> PairingReader for Bn<T> {
     }
 
     fn read_radix_g1<R: Read>(reader: &mut R) -> Result<Self::G1Affine, Error> {
-        let len = <Self::Fq as PrimeField>::BigInt::NUM_LIMBS * 8;
+        let len = <Self::BaseField as PrimeField>::BigInt::NUM_LIMBS * 8;
         let x = {
             let mut b = vec![0; len];
             reader.read_exact(&mut b)?;
             b[0] &= 0x3f;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq x")))
         }?;
         let y = {
             let mut b = vec![0; len];
             reader.read_exact(&mut b)?;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq y")))
         }?;
-        let g1 = Self::G1Affine::new(x, y, false);
+        let g1 = Self::G1Affine::new_unchecked(x, y);
 
         assert!(g1.is_on_curve(), "G1 must be on curve");
 
@@ -92,12 +117,12 @@ impl<T: BnParameters> PairingReader for Bn<T> {
     }
 
     fn read_radix_g2<R: Read>(reader: &mut R) -> Result<Self::G2Affine, Error> {
-        let len = <Self::Fq as PrimeField>::BigInt::NUM_LIMBS * 8;
+        let len = <Self::BaseField as PrimeField>::BigInt::NUM_LIMBS * 8;
         let x1 = {
             let mut b = vec![0; len];
             reader.read_exact(&mut b)?;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq x1")))
         }?;
         let x0 = {
@@ -105,7 +130,7 @@ impl<T: BnParameters> PairingReader for Bn<T> {
             reader.read_exact(&mut b)?;
             b[0] &= 0x3f;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq x0")))
         }?;
         let x = QuadExtField::new(x0, x1);
@@ -113,18 +138,18 @@ impl<T: BnParameters> PairingReader for Bn<T> {
             let mut b = vec![0; len];
             reader.read_exact(&mut b)?;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq y1")))
         }?;
         let y0 = {
             let mut b = vec![0; len];
             reader.read_exact(&mut b)?;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq y0")))
         }?;
         let y = QuadExtField::new(y0, y1);
-        let g2 = Self::G2Affine::new(x, y, false);
+        let g2 = Self::G2Affine::new_unchecked(x, y);
 
         assert!(g2.is_on_curve(), "G2 must be on curve");
 
@@ -132,21 +157,21 @@ impl<T: BnParameters> PairingReader for Bn<T> {
     }
 }
 
-impl<T: Bls12Parameters> PairingReader for Bls12<T> {
+impl<T: Bls12Config> PairingReader for Bls12<T> {
     fn read_ptau_g1<R: Read>(reader: &mut R) -> Result<Self::G1Affine, Error> {
-        let r = Self::Fq::from_repr(<Self::Fq as PrimeField>::Params::R).unwrap();
-        let len = <Self::Fq as PrimeField>::BigInt::NUM_LIMBS * 8;
+        let r = montgomery_r::<Self::BaseField>();
+        let len = <Self::BaseField as PrimeField>::BigInt::NUM_LIMBS * 8;
         let mut b = vec![0; len * 2];
         reader.read_exact(&mut b)?;
 
-        let x = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..len])?)
+        let x = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..len])?)
             .ok_or(Error::Read(String::from("Unable to read Fq x")))?
             .div(r);
-        let y = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[len..])?)
+        let y = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[len..])?)
             .ok_or(Error::Read(String::from("Unable to read Fq y")))?
             .div(r);
 
-        let g1 = Self::G1Affine::new(x, y, false);
+        let g1 = Self::G1Affine::new_unchecked(x, y);
 
         assert!(g1.is_on_curve(), "G1 must be on curve");
 
@@ -154,28 +179,30 @@ impl<T: Bls12Parameters> PairingReader for Bls12<T> {
     }
 
     fn read_ptau_g2<R: Read>(reader: &mut R) -> Result<Self::G2Affine, Error> {
-        let r = Self::Fq::from_repr(<Self::Fq as PrimeField>::Params::R).unwrap();
-        let len = <Self::Fq as PrimeField>::BigInt::NUM_LIMBS * 8;
+        let r = montgomery_r::<Self::BaseField>();
+        let len = <Self::BaseField as PrimeField>::BigInt::NUM_LIMBS * 8;
         let mut b = vec![0; len * 4];
         reader.read_exact(&mut b)?;
 
-        let x0 = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..len])?)
+        let x0 = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..len])?)
             .ok_or(Error::Read(String::from("Unable to read Fq x0")))?
             .div(r);
-        let x1 = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[len..(len * 2)])?)
+        let x1 = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(
+            &b[len..(len * 2)],
+        )?)
             .ok_or(Error::Read(String::from("Unable to read Fq x1")))?
             .div(r);
         let x = QuadExtField::new(x0, x1);
-        let y0 = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(
+        let y0 = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(
             &b[(len * 2)..(len * 3)],
         )?)
         .ok_or(Error::Read(String::from("Unable to read Fq y0")))?
         .div(r);
-        let y1 = Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[(len * 3)..])?)
+        let y1 = Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[(len * 3)..])?)
             .ok_or(Error::Read(String::from("Unable to read Fq y1")))?
             .div(r);
         let y = QuadExtField::new(y0, y1);
-        let g2 = Self::G2Affine::new(x, y, false);
+        let g2 = Self::G2Affine::new_unchecked(x, y);
 
         assert!(g2.is_on_curve(), "G2 must be on curve");
 
@@ -183,23 +210,23 @@ impl<T: Bls12Parameters> PairingReader for Bls12<T> {
     }
 
     fn read_radix_g1<R: Read>(reader: &mut R) -> Result<Self::G1Affine, Error> {
-        let len = <Self::Fq as PrimeField>::BigInt::NUM_LIMBS * 8;
+        let len = <Self::BaseField as PrimeField>::BigInt::NUM_LIMBS * 8;
         let x = {
             let mut b = vec![0; len];
             reader.read_exact(&mut b)?;
             b[0] &= 0x3f;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq x")))
         }?;
         let y = {
             let mut b = vec![0; len];
             reader.read_exact(&mut b)?;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq y")))
         }?;
-        let g1 = Self::G1Affine::new(x, y, false);
+        let g1 = Self::G1Affine::new_unchecked(x, y);
 
         assert!(g1.is_on_curve(), "G1 must be on curve");
 
@@ -207,20 +234,20 @@ impl<T: Bls12Parameters> PairingReader for Bls12<T> {
     }
 
     fn read_radix_g2<R: Read>(reader: &mut R) -> Result<Self::G2Affine, Error> {
-        let len = <Self::Fq as PrimeField>::BigInt::NUM_LIMBS * 8;
+        let len = <Self::BaseField as PrimeField>::BigInt::NUM_LIMBS * 8;
         let x1 = {
             let mut b = vec![0; len];
             reader.read_exact(&mut b)?;
             b[0] &= 0x3f;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq x1")))
         }?;
         let x0 = {
             let mut b = vec![0; len];
             reader.read_exact(&mut b)?;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq x0")))
         }?;
         let x = QuadExtField::new(x0, x1);
@@ -228,18 +255,18 @@ impl<T: Bls12Parameters> PairingReader for Bls12<T> {
             let mut b = vec![0; len];
             reader.read_exact(&mut b)?;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq y1")))
         }?;
         let y0 = {
             let mut b = vec![0; len];
             reader.read_exact(&mut b)?;
             b.reverse();
-            Self::Fq::from_repr(<Self::Fq as PrimeField>::BigInt::read(&b[..])?)
+            Self::BaseField::from_bigint(read_bigint_le::<Self::BaseField>(&b[..])?)
                 .ok_or(Error::Read(String::from("Unable to read Fq y0")))
         }?;
         let y = QuadExtField::new(y0, y1);
-        let g2 = Self::G2Affine::new(x, y, false);
+        let g2 = Self::G2Affine::new_unchecked(x, y);
 
         assert!(g2.is_on_curve(), "G2 must be on curve");
 
